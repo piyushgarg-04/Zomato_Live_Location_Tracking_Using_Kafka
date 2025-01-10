4da11b206d37f91f32dff9a5d8680d97f736e277
Piyush Garg <pg67824@gmail.com>
2025-01-10 13:54:00 +0530

    Add files via upload

---
+++ Coding for Consuming Messages in the EndUser Service/enduser/enduser/src/main/java/com/enduser/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.enduser.constants;
+
+public class AppConstants {
+	
+	public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+	
+	public static final String GROUP_ID="gropu-1";
+
+}
